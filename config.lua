eebee60c880e54ad28bf83f4b7b69071225fb5f8
DCCustomz <dkccustoms5m@gmail.com>
2024-08-06 15:56:58 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,18 +2,17 @@ Config = {
 	framework = 'esx', --esx, qb
 	rollableitems = { -- These are the items you need to have to roll up. It will check for any of these itemsfrom both categories
 		blunts = { -- Blunt Items to load Brown Blunt Prop and Blunt Prop
-			'backwood',
-			--You can add more under here, but make sure to add this same item name to the usable items table below
+			'backwood', --You can add more under this, but make sure to add this same item name to the usable items table below
 		},	
 		papers = { -- Joint Items to load Joint Paper Prop and Joint Prop
-			'rawpaper',
-			--You can add more under here, but make sure to add this same item name to the usable items table below
+			'rawpaper', --You can add more under this, but make sure to add this same item name to the usable items table below
 		}
 	},
 	usableItems = { -- The usable weed items that will initiate roll up process along with the items player will receive after rolling
-		['biscotti_8th'] = {
-			backwood = 'biscotti_backwood', 
-			rawpaper = 'biscotti_joint'}, 
+		['biscotti_8th'] = { --usable items
+			backwood = 'biscotti_backwood', --rollable item from above along with item to receive
+			rawpaper = 'biscotti_joint' --rollable item from above along with item to receive
+		}, 
 		-- Add More here
 	}
 }
